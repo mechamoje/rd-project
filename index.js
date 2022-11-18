5221599d5ccf5d399dcf576ba50778f65051594e
jéssica <jessy.ard11@gmail.com>
2022-11-18 19:33:06 -0300

    hamburguer first commiy

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const menu = document.querySelector("#dropdown-menu");
 const play = document.querySelector(".player");
 const modal = document.querySelector(".modal-wrapper");
 const close = document.querySelector(".modal-close");
+const hamburguerBtn = document.querySelector("#hamburguer-container");
+const navList = document.querySelector(".nav-list");
+const navBar = document.querySelector(".navbar");
 
 navLink.addEventListener("click", () => {
   menu.classList.toggle("menu-open");
@@ -18,4 +21,9 @@ close.addEventListener("click", () => {
 
 modal.addEventListener("click", () => {
   modal.classList.remove("active-modal");
+});
+
+hamburguerBtn.addEventListener("click", () => {
+  navList.classList.toggle("nav-mobile");
+  navBar.classList.toggle("visible-mobile");
 });
