ac1d315034d3f51104e804892e5fdaad77949b06
jéssica <jessy.ard11@gmail.com>
2022-11-20 21:34:04 -0300

    slider

--- index.js
+++ index.js
@@ -214,6 +214,65 @@ function showFeedback(e) {
   }
 }
 
-//mascara telefone
-//after botões
-// li imagem
+const slideBtn1 = document.querySelector(".auto-btn1")
+const slideBtn2 = document.querySelector(".auto-btn2")
+const slideBtn3 = document.querySelector(".auto-btn3")
+const slideBtn4 = document.querySelector(".auto-btn4")
+const slideBtn5 = document.querySelector(".auto-btn5")
+const slideBtn6 = document.querySelector(".auto-btn6")
+const slider = document.querySelector(".slider")
+
+slideBtn1.addEventListener("click", () => {
+  slider.style.transform = 'translateX(55em)'
+  slideBtn1.classList.add("background-btn")
+  slideBtn2.classList.remove("background-btn")
+  slideBtn3.classList.remove("background-btn")
+  slideBtn4.classList.remove("background-btn")
+  slideBtn5.classList.remove("background-btn")
+  slideBtn6.classList.remove("background-btn")
+})
+slideBtn2.addEventListener("click", () => {
+  slider.style.transform = 'translateX(33em)'
+  slideBtn2.classList.add("background-btn")
+  slideBtn1.classList.remove("background-btn")
+  slideBtn3.classList.remove("background-btn")
+  slideBtn4.classList.remove("background-btn")
+  slideBtn5.classList.remove("background-btn")
+  slideBtn6.classList.remove("background-btn")
+} )
+slideBtn3.addEventListener("click",() => {
+  slider.style.transform = 'translateX(11em)'
+  slideBtn3.classList.add("background-btn")
+  slideBtn1.classList.remove("background-btn")
+  slideBtn2.classList.remove("background-btn")
+  slideBtn4.classList.remove("background-btn")
+  slideBtn5.classList.remove("background-btn")
+  slideBtn6.classList.remove("background-btn")
+}  )
+slideBtn4.addEventListener("click",() => {
+  slider.style.transform = 'translateX(-11em)'
+  slideBtn4.classList.add("background-btn")
+  slideBtn1.classList.remove("background-btn")
+  slideBtn2.classList.remove("background-btn")
+  slideBtn3.classList.remove("background-btn")
+  slideBtn5.classList.remove("background-btn")
+  slideBtn6.classList.remove("background-btn")
+}  )
+slideBtn5.addEventListener("click",() => {
+  slider.style.transform = 'translateX(-33em)'
+  slideBtn5.classList.add("background-btn")
+  slideBtn1.classList.remove("background-btn")
+  slideBtn2.classList.remove("background-btn")
+  slideBtn3.classList.remove("background-btn")
+  slideBtn4.classList.remove("background-btn")
+  slideBtn6.classList.remove("background-btn")
+}  )
+slideBtn6.addEventListener("click",() => {
+  slider.style.transform = 'translateX(-55em)'
+  slideBtn6.classList.add("background-btn")
+  slideBtn1.classList.remove("background-btn")
+  slideBtn2.classList.remove("background-btn")
+  slideBtn3.classList.remove("background-btn")
+  slideBtn4.classList.remove("background-btn")
+  slideBtn5.classList.remove("background-btn")
+}  )
